aca2af2cddff2ff1e9ab23a0a5b370aed9cd5096
Hadi Atef <76757642+hady-o@users.noreply.github.com>
2022-08-30 17:09:49 +0200

    home

--- app/src/main/java/com/example/instapp/HomeFragment.java
+++ app/src/main/java/com/example/instapp/HomeFragment.java
@@ -8,12 +8,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_home, container, false);
+        String[] postNames=new String[Post.posts.length];
+        for (int i=0;i<postNames.length;i++){
+            postNames[i]=Post.posts[i].getName();
+        }
+
+        int[] postImages=new int[Post.posts.length];
+        for (int i=0;i<postImages.length;i++){
+            postImages[i]=Post.posts[i].getImageResourceID();
+        }
+        CaptionedImagesAdapter adapter =new CaptionedImagesAdapter(postNames, postImages);
+        RecyclerView res = view.findViewById(R.id.post_recycler);
+
+        LinearLayoutManager mylayoutmanager = new LinearLayoutManager(view.getContext());
+        res.setLayoutManager(mylayoutmanager);
+        res.setAdapter(adapter);
         return view;
     }
 }
